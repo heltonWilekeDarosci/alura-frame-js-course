85ac28b379f05e31755eca566adfe4d3368b54e7
helton <heltoloco@hotmail.com>
2022-08-26 17:49:39 -0300

    feat(app/controllers): create method add with and use method bind

---
+++ client/js/app/controllers/NegotiationController.js
@@ -0,0 +1,11 @@
+class NegotiationController {
+
+    add(event) {
+        event.preventDefault();
+
+        let $ = document.querySelector.bind(document);
+        let inputDate = document.$('#date');
+        let inputQuantity = document.$('#quantity');
+        let inputValue = document.$('#value');
+    }
+}
--- client/js/app/models/Negotiation.js
+++ client/js/app/models/Negotiation.js
@@ -1,12 +1,11 @@
 class Negotiation {
 
     constructor (date, quantity, value) {
-        this._date = new Date(data.getTime()); //Defensive programming to avoid date altering
+        this._date = new Date(data.getTime()); //Defensive programming to avoid date altering//
         this._quantity = quantity;
         this._value = value;
         Object.freeze(this);
     }
-
     
     get date () {
         return new Date(this._date.getTime());
