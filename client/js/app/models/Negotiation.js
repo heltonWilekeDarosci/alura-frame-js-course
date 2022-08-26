f695ad86fa0407f03ee5b671e1dbba5fda0a0cce
helton <heltoloco@hotmail.com>
2022-08-26 10:41:30 -0300

    feat(app/models/Negotiation): create get methods and applied object freeze

--- client/js/app/models/Negotiation.js
+++ client/js/app/models/Negotiation.js
@@ -1,9 +1,26 @@
 class Negotiation {
 
-    constructor () {
-        this.date = new Date();
-        this.quantity = 1;
-        this.value = 0.0;
+    constructor (date, quantity, value) {
+        this._date = date;
+        this._quantity = quantity;
+        this._value = value;
+        Object.freeze(this);
     }
 
+    
+    get date () {
+        return this._date;
+    }
+    
+    get quatity () {
+        return this._quantity;
+    }
+    
+    get value () {
+        return this._value;
+    }
+    
+    get volume() {
+        return this._quantity * this._value;
+    }
 }
