e9bb59ccc04f80d2dba069aa03e3ee84d787f541
helton <heltoloco@hotmail.com>
2022-08-26 18:47:06 -0300

    feat(app/controllers): finish negotiation controller

--- client/js/app/controllers/NegotiationController.js
+++ client/js/app/controllers/NegotiationController.js
@@ -1,11 +1,25 @@
 class NegotiationController {
 
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        this._inputDate = $('#date');
+        this._inputQuantity = $('#quantity');
+        this._inputValue = $('#value');
+    }
+    
     add(event) {
         event.preventDefault();
-
-        let $ = document.querySelector.bind(document);
-        let inputDate = document.$('#date');
-        let inputQuantity = document.$('#quantity');
-        let inputValue = document.$('#value');
+        
+        let date = new Date(
+            ...this._inputDate.value
+                .split('-')
+                .map((item, index) => item - index % 2)
+        );
+                
+        let negotiation = new Negotiation(
+            date,
+            this._inputQuantity.value,
+            this._inputValue.value,
+        );
     }
 }
