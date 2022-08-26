34f33f125c6b5653c59edb6ec2ff25f212030874
helton <heltoloco@hotmail.com>
2022-08-26 20:28:16 -0300

    feat(app/models): create negotiation list class

--- client/js/app/controllers/NegotiationController.js
+++ client/js/app/controllers/NegotiationController.js
@@ -5,15 +5,28 @@ class NegotiationController {
         this._inputDate = $('#date');
         this._inputQuantity = $('#quantity');
         this._inputValue = $('#value');
+        this._negotiationList = new NegotiationList();
     }
     
     add(event) {
         event.preventDefault();
                 
-        let negotiation = new Negotiation(
+        this._negotiationList.add(this._createNegotiation());
+        this._clearForm();
+    }
+    
+    _createNegotiation() {
+        return new Negotiation(
             DateHelper.textToDate(this._inputDate.value),
             this._inputQuantity.value,
             this._inputValue.value,
         );
+    }
+
+    _clearForm() {
+        this._inputDate.value = '';
+        this._inputQuantity.value = 1;
+        this._inputValue.value = 0.0;
+        this._inputDate.focus();
     }
 }
